cc7166aadf56ae6c57287aa4306ce914c42958db
Callan Bryant <callan.bryant@gmail.com>
2011-07-28 14:41:54 +0100

    flush index/cache and crawl

--- cli/flushIndex.sh
+++ cli/flushIndex.sh
@@ -4,15 +4,9 @@
 require_once __DIR__.'/../kernel.class.php';
 kernel::bootstrap();
 
-if (!isset($argv[1]))
-	die("Usage $argv[0] <directory>\n");
-
-
 $i = indexer::getInstance();
 $c = new mp3Crawler($i);
 
-$c->crawl($argv[1]);
-
-echo "\n$c->count files indexed\n";
+$i->flush();
 
 ?>
