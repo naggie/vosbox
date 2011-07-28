66527bfab3f08d385729b1693a2eeeb8a8d16bd8
Callan Bryant <callan.bryant@gmail.com>
2011-07-28 10:02:03 +0100

    error now thrown if json ext no loaded

--- search.node.php
+++ search.node.php
@@ -1,12 +1,11 @@
 <?php
-/*
-TODO: exception based JSON error system
-*/
-
 $keywords = &$_REQUEST['keywords'];
 
 try
 {
+	if (!extension_loaded('json'))
+		throw new Exception('json extension not loaded');
+
 	if (!$keywords)
 		throw new Exception('Erm, please search for something!');
 
@@ -15,7 +14,10 @@ try
 }
 catch (Exception $e)
 {
-	$response = array('error' => $e->getMessage() );
+	// manually throw the error, as the json ext may not be loaded
+	header('Content-Type:application/json');
+	echo '{"error":"'.$e->getMessage().'"}';
+	die();
 }
 
 header('Content-Type:application/json');
