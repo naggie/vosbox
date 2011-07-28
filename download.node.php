240ccb904fe4e32ee26d94a2a3793a6cbd42d5cd
Callan Bryant <callan.bryant@gmail.com>
2011-07-28 09:55:15 +0100

    upon downloading a file that no longer exists, the record of the object is depreciated

--- download.node.php
+++ download.node.php
@@ -6,6 +6,9 @@ $i = indexer::getInstance();
 
 $filepath = $i->getObject($id)->path;
 
+if (!file_exists($filepath) )
+	$i->depreciateObject($id);
+
 $response = new httpResponse();
 $response->load_local_file($filepath);
 
