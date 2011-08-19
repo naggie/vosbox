587d5f4b7be8098fabeca24863df389ffb035b51
Callan Bryant <callan.bryant@gmail.com>
2011-08-19 11:29:23 +0100

    fixed flushcached (required old vw kernel)

--- cli/flushCache.sh
+++ cli/flushCache.sh
@@ -1,31 +1,7 @@
 #!/usr/bin/php
 <?php
-/**
- * invalidates all cache objects. Useful after an upgrade.
- *
- *     SCMwiki Copyright (C) 2010  Callan Bryant <callan1990@googlemail.com>
- *
- *     Based on Voswork - A simple, fast PHP filesystem abstraction layer
- *     Voswork Copyright (C) 2010  Callan Bryant <callan1990@googlemail.com>
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @package main
- * @author Callan Bryant <callan1990@googlemail.com>
- */
-
+// flushes cache
 require_once __DIR__.'/../cache.class.php';
+
 cache::getInstance()->flush();
 ?>
