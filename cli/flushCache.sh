e1fbbf439cbe741ae38a7443e217c529444926ac
Callan Bryant <callan.bryant@gmail.com>
2011-11-15 22:03:20 +0000

    vosplayer -> vosbox in license boilerplates

--- cli/flushCache.sh
+++ cli/flushCache.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/php
 <?php
 /*
-    This file is part of Vosplayer.
+    This file is part of Vosbox.
 
-    Vosplayer is free software: you can redistribute it and/or modify
+    Vosbox is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Vosplayer is distributed in the hope that it will be useful,
+    Vosbox is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
-    Vosplayer copyright Callan Bryant 2011 <callan.bryant@gmail.com>
+    Vosbox copyright Callan Bryant 2011 <callan.bryant@gmail.com>
 */
 
 // flushes cache
--- cli/flushIndex.sh
+++ cli/flushIndex.sh
@@ -2,14 +2,14 @@
 <?php
 
 /*
-    This file is part of Vosplayer.
+    This file is part of Vosbox.
 
-    Vosplayer is free software: you can redistribute it and/or modify
+    Vosbox is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Vosplayer is distributed in the hope that it will be useful,
+    Vosbox is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
-    Vosplayer copyright Callan Bryant 2011 <callan.bryant@gmail.com>
+    Vosbox copyright Callan Bryant 2011 <callan.bryant@gmail.com>
 */
 
 // flushes index (including cache) and playlists -- not album art
